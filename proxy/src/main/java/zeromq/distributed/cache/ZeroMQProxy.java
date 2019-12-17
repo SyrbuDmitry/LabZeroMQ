9e4123366fdefe0d783454534318092a7c4a48f3
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-17 23:43:13 +0300

    Scripted auto-commit on change (2019-12-17 23:43:13) by gitwatch.sh

--- proxy/src/main/java/zeromq/distributed/cache/ZeroMQProxy.java
+++ proxy/src/main/java/zeromq/distributed/cache/ZeroMQProxy.java
@@ -42,11 +42,12 @@ public class ZeroMQProxy {
                     message = backend.recv(0);
                     String strMsg = new String(message);
                     String [] msg = parseString(strMsg);
-                    if(msg[0].equals("NOTIFY"))
-                        serverList.add(new CacheSegment(Integer.parseInt(msg[1]),Integer.parseInt(msg[2])));
-
+                    if(msg[0].equals("NOTIFY")) {
+                        CacheSegment insert = new CacheSegment(Integer.parseInt(msg[1]), Integer.parseInt(msg[2]);
+                        if(!serverList.contains(insert))
+                            serverList.add(new CacheSegment(Integer.parseInt(msg[1]), Integer.parseInt(msg[2])));
+                    }
                     more = backend.hasReceiveMore();
-
                     frontend.send(message, more ? ZMQ.SNDMORE : 0);
                     if (!more) {
                         break;
