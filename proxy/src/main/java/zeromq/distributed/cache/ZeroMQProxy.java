3da7a9c8e022c8bd74243ec85b1d5858185e5eb9
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-17 23:11:13 +0300

    Scripted auto-commit on change (2019-12-17 23:11:13) by gitwatch.sh

--- proxy/src/main/java/zeromq/distributed/cache/ZeroMQProxy.java
+++ proxy/src/main/java/zeromq/distributed/cache/ZeroMQProxy.java
@@ -1,27 +1,47 @@
 package zeromq.distributed.cache;
 
+import org.zeromq.SocketType;
+import org.zeromq.ZMQ;
+
 public class ZeroMQProxy {
-    public static void main(String[] args){
-        Context context = ZMQ.context(1);
-        Socket frontend = context.socket(SocketType.ROUTER);
-        Socket backend = context.socket(SocketType.DEALER);
-        frontend.bind("tcp://*:5559"); backend.bind("tcp://*:5560");
+    public static void main(String[] args) {
+        ZMQ.Context context = ZMQ.context(1);
+        ZMQ.Socket frontend = context.socket(SocketType.ROUTER);
+        ZMQ.Socket backend = context.socket(SocketType.ROUTER);
+        frontend.bind("tcp://*:5559");
+        backend.bind("tcp://*:5560");
         System.out.println("launch and connect broker.");
 // Initialize poll set
-        Poller items = context.poller (2); items.register(frontend, Poller.POLLIN); items.register(backend, Poller.POLLIN); boolean more = false;
+        ZMQ.Poller items = context.poller(2);
+        items.register(frontend, ZMQ.Poller.POLLIN);
+        items.register(backend, ZMQ.Poller.POLLIN);
+        boolean more = false;
         byte[] message;
 // Switch messages between sockets
         while (!Thread.currentThread().isInterrupted()) {
-// poll and memorize multipart detection items.poll();
+// poll and memorize multipart detection
+            items.poll();
+            if (items.pollin(0)) {
+                while (true) {
+                    message = frontend.recv(0);
+                    more = frontend.hasReceiveMore();
+                    backend.send(message, more ? ZMQ.SNDMORE : 0);
+                    if (!more) {
+                        break;
+                    }
+                }
+            }
+            if (items.pollin(1)) {
+                while (true) {
+                    message = backend.recv(0);
+                    more = backend.hasReceiveMore();
+                    frontend.send(message, more ? ZMQ.SNDMORE : 0);
+                    if (!more) {
+                        break;
+                    }
+                }
+            }
+
+        }
+
     }
-        if (items.pollin(0)) { while (true) {
-            message = frontend.recv(0);
-            more = frontend.hasReceiveMore(); backend.send(message, more ? ZMQ.SNDMORE : 0); if(!more){
-                break; }
-        } }
-        if (items.pollin(1)) { while (true) {
-            message = backend.recv(0);
-            more = backend.hasReceiveMore(); frontend.send(message, more ? ZMQ.SNDMORE : 0); if(!more){
-                break; }
-        } }
-}
