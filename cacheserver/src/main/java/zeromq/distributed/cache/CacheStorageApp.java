c88278219f9825453093b24ba9bbefc876d39009
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-19 00:39:14 +0300

    Scripted auto-commit on change (2019-12-19 00:39:13) by gitwatch.sh

--- cacheserver/src/main/java/zeromq/distributed/cache/CacheStorageApp.java
+++ cacheserver/src/main/java/zeromq/distributed/cache/CacheStorageApp.java
@@ -5,22 +5,19 @@ import org.zeromq.ZMQ;
 
 public class CacheStorageApp {
     public static void main(String[] args) {
-        ZMQ.Context context = ZMQ.context(1);
+        ZMQ.Context context = ZMQ.context (1);
 // Socket to talk to server
-        ZMQ.Socket responder = context.socket(SocketType.REP);
-        responder.connect("tcp://localhost:5550");
-        System.out.println("CONNECTED");
-        while (!Thread.currentThread().isInterrupted()) {
+        ZMQ.Socket responder = context.socket (SocketType.REP); responder.connect ("tcp://localhost:5560");
+        while (!Thread.currentThread ().isInterrupted ()) {
 // Wait for next request from client
-            String string = responder.recvStr(0);
-            System.out.printf("Received request: [%s]\n", string); // Do some 'work'
+            String string = responder.recvStr (0); System.out.printf ("Received request: [%s]\n", string); // Do some 'work'
             try {
-                Thread.sleep(1000);
+                Thread.sleep (1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 // Send reply back to client
-            responder.send("World");
+            responder.send ("World");
         }
 // We never get here but clean up anyhow
         responder.close();
