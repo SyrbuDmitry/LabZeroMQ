426c8837c4a76b11be47655d1a74e1bdad7ae9b8
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-19 02:20:26 +0300

    Scripted auto-commit on change (2019-12-19 02:20:26) by gitwatch.sh

--- proxy/src/main/java/zeromq/distributed/cache/ZeroMQProxy.java
+++ proxy/src/main/java/zeromq/distributed/cache/ZeroMQProxy.java
@@ -13,63 +13,38 @@ public class ZeroMQProxy {
     public static void main(String[] args) {
         ZMQ.Context context = ZMQ.context(1);
         ZMQ.Socket frontend = context.socket(SocketType.ROUTER);
-        ZMQ.Socket backend = context.socket(SocketType.ROUTER);
+        ZMQ.Socket backend = context.socket(SocketType.DEALER);
         frontend.bind("tcp://*:5559");
         backend.bind("tcp://*:5560");
-
         System.out.println("launch and connect broker.");
 // Initialize poll set
         ZMQ.Poller items = context.poller(2);
         items.register(frontend, ZMQ.Poller.POLLIN);
         items.register(backend, ZMQ.Poller.POLLIN);
         boolean more = false;
-        byte[] message,id,dil,server=null,client=null;
+        byte[] message;
 // Switch messages between sockets
         while (!Thread.currentThread().isInterrupted()) {
-// poll and memorize multipart detection
-            items.poll();
+// poll and memorize multipart detection items.poll();
             if (items.pollin(0)) {
-                System.out.println();
-                System.out.println("REQ");
-//                while (true) {
-                    id = frontend.recv(0);
-                    dil = frontend.recv(0);
+                while (true) {
                     message = frontend.recv(0);
-                    client = id;
-                    System.out.println(new String(id));
-                    System.out.println(new String(dil));
-                    System.out.println(new String(message));
-                    backend.sendMore(server);
-                    backend.sendMore(dil);
-                    backend.send(message,0);
-
-//                    if (!more) {
-//                        break;
-//                    }
-//                }
+                    more = frontend.hasReceiveMore();
+                    backend.send(message, more ? ZMQ.SNDMORE : 0);
+                    if (!more) {
+                        break;
+                    }
+                }
             }
             if (items.pollin(1)) {
-                System.out.println();
-                System.out.println("REP");
-                //while (true) {
-                id = backend.recv(0);
-                dil = backend.recv(0);
-                message = backend.recv(0);
-                System.out.println(new String(id));
-                System.out.println(new String(dil));
-                System.out.println(new String(message));
-                if(new String(message).equals("N"))
-                    server = id;
-                else {
-                    frontend.sendMore(client);
-                    frontend.sendMore("")
+                while (true) {
+                    message = backend.recv(0);
+                    more = backend.hasReceiveMore();
+                    frontend.send(message, more ? ZMQ.SNDMORE : 0);
+                    if (!more) {
+                        break;
+                    }
                 }
-//                   more = backend.hasReceiveMore();
-                    //frontend.send(message, more ? ZMQ.SNDMORE : 0);
-//                    if (!more) {
-//                        break;
-//                    }
-                //}
             }
         }
     }
