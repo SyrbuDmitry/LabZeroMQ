8cf421546294f29afb69013a3c18d424839e4d65
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-18 23:47:28 +0300

    Scripted auto-commit on change (2019-12-18 23:47:28) by gitwatch.sh

--- client/src/main/java/zeromq/distributed/cache/rtdealer.java
+++ client/src/main/java/zeromq/distributed/cache/rtdealer.java
@@ -26,7 +26,9 @@ public class rtdealer
         requester.connect("tcp://localhost:5559");
         System.out.println("launch and connect client.");
             requester.send("PUT 0 A", 0);
+            requester.recvStr(0);
             requester.send("PUT 1 B", 0);
+
             requester.send("GET 0", 0);
             String message = requester.recvStr(0);
             System.out.println("RESPONSE");
