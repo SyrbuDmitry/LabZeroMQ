21fbd8d7e39caf3c49b1fcafd03b7de484f4206d
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-19 02:08:40 +0300

    Scripted auto-commit on change (2019-12-19 02:08:40) by gitwatch.sh

--- proxy/src/main/java/zeromq/distributed/cache/ZeroMQProxy.java
+++ proxy/src/main/java/zeromq/distributed/cache/ZeroMQProxy.java
@@ -13,61 +13,15 @@ public class ZeroMQProxy {
     public static void main(String[] args) {
         ZMQ.Context context = ZMQ.context(1);
         ZMQ.Socket frontend = context.socket(SocketType.ROUTER);
-        ZMQ.Socket backend = context.socket(SocketType.ROUTER);
-        frontend.bind("tcp://*:5559");
-        backend.bind("tcp://*:5560");
-
+        ZMQ.Socket backend = context.socket(SocketType.DEALER);
+        frontend.bind("tcp://*:5559"); backend.bind("tcp://*:5560");
         System.out.println("launch and connect broker.");
 // Initialize poll set
-        ZMQ.Poller items = context.poller(2);
-        items.register(frontend, ZMQ.Poller.POLLIN);
-        items.register(backend, ZMQ.Poller.POLLIN);
-        boolean more = false;
-        byte[] message,id,dil,server=null,client=null;
+        ZMQ.Poller items = context.poller (2); items.register(frontend, ZMQ.Poller.POLLIN); items.register(backend, ZMQ.Poller.POLLIN); boolean more = false;
+        byte[] message;
 // Switch messages between sockets
         while (!Thread.currentThread().isInterrupted()) {
 // poll and memorize multipart detection
             items.poll();
-            if (items.pollin(0)) {
-
-                System.out.println("REQ");
-//                while (true) {
-                    id = frontend.recv(0);
-                    dil = frontend.recv(0);
-                    message = frontend.recv(0);
-                    client = id;
-                    System.out.println();
-                    backend.sendMore(server);
-                    backend.sendMore(dil);
-                    backend.send(message,0);
-
-//                    if (!more) {
-//                        break;
-//                    }
-//                }
-            }
-            if (items.pollin(1)) {
-
-                //while (true) {
-                id = backend.recv(0);
-                dil = backend.recv(0);
-                message = backend.recv(0);
-
-                if(new String(message).equals("N"))
-                    server = id;
-                else {
-                    System.out.printf ("Received response: [%s]\n", new String(message));
-                    frontend.sendMore(client);
-                    frontend.sendMore("");
-                    frontend.send(message);
-                }
-//                   more = backend.hasReceiveMore();
-                    //frontend.send(message, more ? ZMQ.SNDMORE : 0);
-//                    if (!more) {
-//                        break;
-//                    }
-                //}
-            }
-        }
     }
 }
