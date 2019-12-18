06c18cca0b482bce7dcfec4fb8dfb995dbb92a57
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-19 02:09:25 +0300

    Scripted auto-commit on change (2019-12-19 02:09:25) by gitwatch.sh

--- client/src/main/java/zeromq/distributed/cache/rtdealer.java
+++ client/src/main/java/zeromq/distributed/cache/rtdealer.java
@@ -20,13 +20,12 @@ public class rtdealer {
     public static void main(String[] args) throws Exception {
         ZMQ.Context context = ZMQ.context(1);
 // Socket to talk to server
-        Socket requester = context.socket(SocketType.REQ);
-        requester.connect("tcp://localhost:5559");
-        System.out.println("launch and connect client.");
+        Socket requester = context.socket(SocketType.REQ); requester.connect("tcp://localhost:5559"); System.out.println("launch and connect client.");
+        for (int request_nbr = 0; request_nbr < 10; request_nbr++) {
             requester.send("Hello", 0);
-            String reply = requester.recvStr(0);
-            System.out.println("Received reply " + " [" + reply + "]");
-
+            String reply = requester.recvStr (0);
+            System.out.println("Received reply " + request_nbr + " [" + reply + "]");
+        }
 // We never get here but clean up anyhow
         requester.close();
         context.term();
