9ff69528aac35348b3bb7119db2749cba58a5f59
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-19 00:10:38 +0300

    Scripted auto-commit on change (2019-12-19 00:10:38) by gitwatch.sh

--- cacheserver/src/main/java/zeromq/distributed/cache/CacheStorageApp.java
+++ cacheserver/src/main/java/zeromq/distributed/cache/CacheStorageApp.java
@@ -19,7 +19,7 @@ public class CacheStorageApp {
                 e.printStackTrace();
             }
 // Send reply back to client
-            //responder.send ("World");
+            responder.send ("World");
         }
 // We never get here but clean up anyhow
         responder.close();
