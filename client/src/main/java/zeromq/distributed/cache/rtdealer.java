28a89773f072f5e79d0110046aeebcebaf9a2e74
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-18 22:53:11 +0300

    Scripted auto-commit on change (2019-12-18 22:53:11) by gitwatch.sh

--- client/src/main/java/zeromq/distributed/cache/rtdealer.java
+++ client/src/main/java/zeromq/distributed/cache/rtdealer.java
@@ -23,7 +23,9 @@ public class rtdealer
     {
         ZMQ.Context context = ZMQ.context(1);
 // Socket to talk to server
-        Socket requester = context.socket(SocketType.REQ); requester.connect("tcp://localhost:5559"); System.out.println("launch and connect client.");
+        Socket requester = context.socket(SocketType.REQ);
+        requester.connect("tcp://localhost:5559");
+        System.out.println("launch and connect client.");
         for (int request_nbr = 0; request_nbr < 10; request_nbr++) {
             requester.send("Hello", 0);
             String reply = requester.recvStr (0);
