8a461e2e9023042b44a8fb9ffd8896fc4c643387
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-19 00:23:35 +0300

    Scripted auto-commit on change (2019-12-19 00:23:35) by gitwatch.sh

--- cacheserver/src/main/java/zeromq/distributed/cache/CacheStorageApp.java
+++ cacheserver/src/main/java/zeromq/distributed/cache/CacheStorageApp.java
@@ -4,32 +4,32 @@ import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
 
 public class CacheStorageApp {
-    public static void main(String[] args){
-        ZMQ.Context context = ZMQ.context (1);
+    public static void main(String[] args) {
+        ZMQ.Context context = ZMQ.context(1);
 // Socket to talk to server
-        ZMQ.Socket dealer = context.socket (SocketType.DEALER);
+        ZMQ.Socket dealer = context.socket(SocketType.DEALER);
         dealer.connect("tcp://localhost:5560");
         System.out.println("bind to localhost:5560");
 //        dealer.sendMore("");
 //        dealer.send("NOTIFY 0 5");
         Storage cache = new Storage();
         System.out.println("START LISTENING TO POLLS");
-        while (!Thread.currentThread().isInterrupted ()) {
+        while (!Thread.currentThread().isInterrupted()) {
             String req = dealer.recvStr();
-            System.out.printf ("Received request: [%s]\n", req); // Do some 'work'
+            System.out.printf("Received request: [%s]\n", req); // Do some 'work'
             try {
-                Thread.sleep (1000);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            String [] reqPar = req.split(" ");
-            if(reqPar[0].equals("GET")){
+            String[] reqPar = req.split(" ");
+            if (reqPar[0].equals("GET")) {
                 dealer.sendMore("");
                 dealer.send(cache.getValue(Integer.parseInt(reqPar[1])));
             }
-           if(reqPar[0].equals("PUT")){
+            if (reqPar[0].equals("PUT")) {
                 System.out.println("REQUEST PUT FROM CLIENT");
-                cache.putValue(Integer.parseInt(reqPar[1]),reqPar[2]);
+                cache.putValue(Integer.parseInt(reqPar[1]), reqPar[2]);
                 dealer.send("");
             }
         }
