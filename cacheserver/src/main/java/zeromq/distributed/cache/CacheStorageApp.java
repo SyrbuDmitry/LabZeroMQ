2294b597b182f4bb685d63e0009266f3f30381c6
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-19 02:24:35 +0300

    Scripted auto-commit on change (2019-12-19 02:24:35) by gitwatch.sh

--- cacheserver/src/main/java/zeromq/distributed/cache/CacheStorageApp.java
+++ cacheserver/src/main/java/zeromq/distributed/cache/CacheStorageApp.java
@@ -10,7 +10,8 @@ public class CacheStorageApp {
         ZMQ.Socket responder = context.socket (SocketType.REP); responder.connect ("tcp://localhost:5560");
         while (!Thread.currentThread ().isInterrupted ()) {
 // Wait for next request from client
-            String string = responder.recvStr (0); System.out.printf ("Received request: [%s]\n", string); // Do some 'work'
+            String string = responder.recvStr (0);
+            System.out.printf ("Received request: [%s]\n", string); // Do some 'work'
             try {
                 Thread.sleep (1000);
             } catch (InterruptedException e) {
