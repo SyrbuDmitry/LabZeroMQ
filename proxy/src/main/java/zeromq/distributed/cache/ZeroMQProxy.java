d68c02a1037265ed52d4caeff8a3f1cbb78add53
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-19 00:08:31 +0300

    Scripted auto-commit on change (2019-12-19 00:08:31) by gitwatch.sh

--- proxy/src/main/java/zeromq/distributed/cache/ZeroMQProxy.java
+++ proxy/src/main/java/zeromq/distributed/cache/ZeroMQProxy.java
@@ -29,16 +29,15 @@ public class ZeroMQProxy {
                 items.poll();
 // poll and memorize multipart detection items.poll();
             if (items.pollin(0)) {
-                System.out.println("REQUEST");
-                identity = frontend.recvStr();
-                frontend.recvStr();
-                message = frontend.recvStr();
-                System.out.println(message);
+                while (true) {
+                    message = frontend.recvStr(0);
+                    more = frontend.hasReceiveMore();
+                    backend.send(message, more ? ZMQ.SNDMORE : 0);
+                    if (!more) {
+                        break;
+                    }
 
-                backend.sendMore(identity);
-                backend.sendMore("");
-                backend.send(message);
-
+                }
             }
             if (items.pollin(1)) {
                 System.out.println("RESPONSE");
