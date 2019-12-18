3c683c65d4ad5b2ee71d585e61ea17a9a00a78a8
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-18 22:46:23 +0300

    Scripted auto-commit on change (2019-12-18 22:46:23) by gitwatch.sh

--- cacheserver/src/main/java/zeromq/distributed/cache/CacheStorageApp.java
+++ cacheserver/src/main/java/zeromq/distributed/cache/CacheStorageApp.java
@@ -7,24 +7,18 @@ public class CacheStorageApp {
     public static void main(String[] args){
         ZMQ.Context context = ZMQ.context (1);
 // Socket to talk to server
-        ZMQ.Socket dealer = context.socket (SocketType.DEALER);
-        dealer.bind("tcp://*:5560");
-        System.out.println("bind to localhost:5560");
-        Storage cache = new Storage();
+        ZMQ.Socket responder = context.socket (SocketType.REP); responder.connect ("tcp://localhost:5560");
         while (!Thread.currentThread ().isInterrupted ()) {
 // Wait for next request from client
-            String req = dealer.recvStr (0);
-            System.out.printf ("Received request: [%s]\n", req); // Do some 'work'
+            String string = responder.recvStr (0);
+            System.out.printf ("Received request: [%s]\n", string); // Do some 'work'
             try {
                 Thread.sleep (1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            // Send reply back to client
-            if(req.equals("get"))
-                dealer.send (cache.getValue(1).getBytes(ZMQ.CHARSET),0);
-            if(req.equals("put"))
-                cache.putValue(4,"D");
+// Send reply back to client
+            responder.send ("World");
         }
 // We never get here but clean up anyhow responder.close();
         context.term();
