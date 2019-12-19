6ed6cf9415c058b8267746185b46558b995d2418
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-19 13:03:46 +0300

    Scripted auto-commit on change (2019-12-19 13:03:46) by gitwatch.sh

--- cacheserver/src/main/java/zeromq/distributed/cache/CacheStorageApp.java
+++ cacheserver/src/main/java/zeromq/distributed/cache/CacheStorageApp.java
@@ -21,8 +21,8 @@ public class CacheStorageApp {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("REQ FROM CLIENT");
-            System.out.println("M: "+responder.recvStr());
+
+            System.out.println("DIL: "+responder.recvStr());
             client = responder.recvStr();
             System.out.println("CLIENT "+ client);
             responder.recv(0);
