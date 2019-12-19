81a105384d62ecd2cd86b44121a69ea7445f378a
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-19 12:24:36 +0300

    Scripted auto-commit on change (2019-12-19 12:24:36) by gitwatch.sh

--- cacheserver/src/main/java/zeromq/distributed/cache/CacheStorageApp.java
+++ cacheserver/src/main/java/zeromq/distributed/cache/CacheStorageApp.java
@@ -7,19 +7,22 @@ public class CacheStorageApp {
     public static void main(String[] args) {
         ZMQ.Context context = ZMQ.context (1);
 // Socket to talk to server
-        ZMQ.Socket responder = context.socket (SocketType.REP);
+        ZMQ.Socket responder = context.socket (SocketType.DEALER);
         responder.connect ("tcp://localhost:5560");
+        responder.sendMore("");
+        responder.send("N");
         while (!Thread.currentThread ().isInterrupted ()) {
 // Wait for next request from client
-            String string = responder.recvStr (0);
-            System.out.printf ("Received request: [%s]\n", string); // Do some 'work'
+
             try {
                 Thread.sleep (1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-// Send reply back to client
-            responder.send ("World");
+            responder.recv(0);
+            responder.sendMore(responder.recv(0));
+            responder.sendMore("");
+            responder.send("World");
         }
 // We never get here but clean up anyhow
         responder.close();
--- proxy/src/main/java/zeromq/distributed/cache/ZeroMQProxy.java
+++ proxy/src/main/java/zeromq/distributed/cache/ZeroMQProxy.java
@@ -13,7 +13,7 @@ public class ZeroMQProxy {
     public static void main(String[] args) {
         ZMQ.Context context = ZMQ.context(1);
         ZMQ.Socket frontend = context.socket(SocketType.ROUTER);
-        ZMQ.Socket backend = context.socket(SocketType.DEALER);
+        ZMQ.Socket backend = context.socket(SocketType.ROUTER);
         frontend.bind("tcp://*:5559");
         backend.bind("tcp://*:5560");
         System.out.println("launch and connect broker.");
@@ -22,35 +22,40 @@ public class ZeroMQProxy {
         items.register(frontend, ZMQ.Poller.POLLIN);
         items.register(backend, ZMQ.Poller.POLLIN);
         boolean more = false;
-        byte[] message;
+        byte[] message,client=null,server=null;
 // Switch messages between sockets
         while (!Thread.currentThread().isInterrupted()) {
 // poll and memorize multipart detection
             items.poll();
             if (items.pollin(0)) {
                 System.out.println("REQUEST");
-                while (true) {
+
+                    client = frontend.recv(0);
+
+                    frontend.recv(0);
                     message = frontend.recv(0);
-                    System.out.println(Arrays.toString(message));
-                    more = frontend.hasReceiveMore();
-                    backend.send(message, more ? ZMQ.SNDMORE : 0);
-                    if (!more) {
-                        break;
-                    }
-                }
+                    System.out.println(new String(client));
+                    
+                    backend.sendMore(server);
+                    backend.sendMore("");
+                    backend.sendMore(client);
+                    backend.sendMore("");
+                    backend.send(message);
+
                 System.out.println();
             }
             if (items.pollin(1)) {
-                System.out.println("RESPONSE");
-                while (true) {
+
+                    server = backend.recv(0);
+                    backend.recv(0);
                     message = backend.recv(0);
-                    System.out.println(Arrays.toString(message));
-                    more = backend.hasReceiveMore();
-                    frontend.send(message, more ? ZMQ.SNDMORE : 0);
-                    if (!more) {
-                        break;
-                    }
-                }
+//                    if (new String(server).equals("N"))
+//                    System.out.println(Arrays.toString(message));
+//                    more = backend.hasReceiveMore();
+//                    frontend.send(message, more ? ZMQ.SNDMORE : 0);
+//                    if (!more) {
+//                        break;
+//                    }
                 System.out.println();
             }
         }
