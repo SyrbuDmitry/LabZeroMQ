1c1b279b3d17b68fca4ad3b6f0830033e0206b8b
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-19 14:01:46 +0300

    Scripted auto-commit on change (2019-12-19 14:01:46) by gitwatch.sh

--- cacheserver/src/main/java/zeromq/distributed/cache/CacheStorageApp.java
+++ cacheserver/src/main/java/zeromq/distributed/cache/CacheStorageApp.java
@@ -23,7 +23,7 @@ public class CacheStorageApp {
         while (!Thread.currentThread().isInterrupted()) {
 // Wait for next request from client
 
-            System.out.println(items.poll());
+           
             if (items.pollin(0)) {
                 System.out.println("POLL");
                 responder.recvStr();
